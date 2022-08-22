b1788ee7d231dd736bf8fafadea4bed5c50c59e9
eszterberenyi <e.eszterberenyi@gmail.com>
2022-08-22 15:20:59 +0200

    add html templates and css file

--- data_manager.py
+++ data_manager.py
@@ -1,23 +1,21 @@
 from psycopg2 import sql
-from datetime import datetime
-
 import database_connection
 
 
-@database_connection.connection_handler
-def insert_data(cursor):
-    query = """
-        INSERT INTO users(name) VALUES ('Tom');
-    """
-    cursor.execute(query)
-
-insert_data()
-
-@database_connection.connection_handler
-def insert_score(cursor):
-    query = f"""
-        INSERT INTO scores(users_id, score) VALUES (1, 55);
-    """
-    cursor.execute(query)
-
-insert_score()
+# @database_connection.connection_handler
+# def insert_data(cursor):
+#     query = """
+#         INSERT INTO users(name) VALUES ('Tom');
+#     """
+#     cursor.execute(query)
+#
+# insert_data()
+#
+# @database_connection.connection_handler
+# def insert_score(cursor):
+#     query = f"""
+#         INSERT INTO scores(users_id, score) VALUES (1, 55);
+#     """
+#     cursor.execute(query)
+#
+# insert_score()
