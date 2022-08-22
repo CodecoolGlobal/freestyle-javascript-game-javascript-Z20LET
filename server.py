0f4854bf2c2efd5e130cffe587e02362fab70235
eszterberenyi <e.eszterberenyi@gmail.com>
2022-08-22 13:19:03 +0200

    create project structure

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from psycopg2.errors import UniqueViolation
+from datetime import timedelta
+import data_manager
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = b'\x1dH@\xb94\xc9\xb0\x8e\xd5\xa8\xfe\\r\x00\x0c\xb4'
+app.permanent_session_lifetime = timedelta(minutes=30)
+
+
+@app.route("/")
+def index():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        port=5000
+    )
+
