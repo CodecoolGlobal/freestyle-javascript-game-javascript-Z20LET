b76e5921e55b8e5beed2d1ba375dd76072db3ef7
juhaszszabolcs90 <juhaszszabolcs90@gmail.com>
2022-08-23 10:05:02 +0200

    display high scores

--- server.py
+++ server.py
@@ -11,6 +11,11 @@ app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.route("/")
 def index():
+    return render_template('index.html')
+
+
+@app.route("/game")
+def game():
     return render_template('game.html')
 
 
