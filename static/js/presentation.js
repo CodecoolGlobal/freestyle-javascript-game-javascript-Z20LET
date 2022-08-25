e682f73953ea729f44403403d1f7ddcbbaa901c7
eszterberenyi <e.eszterberenyi@gmail.com>
2022-08-25 20:25:03 +0200

    add presentation page layout and functionality

---
+++ static/js/presentation.js
@@ -0,0 +1,31 @@
+const title = document.querySelector('#h1');
+const workFlow = document.querySelector('#workflow');
+const codeSnippets = document.querySelector('#snippets');
+const snippetOne = document.querySelector('#snippet1');
+const snippetTwo = document.querySelector('#snippet2');
+const startGame = document.querySelector('a')
+
+title.addEventListener('click', (e) => {
+     workFlow.classList.remove('inactive')
+});
+
+workFlow.addEventListener('click', (e) => {
+     codeSnippets.classList.remove('inactive')
+});
+
+codeSnippets.addEventListener('click', (e) => {
+     title.classList.add('inactive');
+     workFlow.classList.add('inactive');
+     codeSnippets.classList.add('inactive');
+     snippetOne.classList.remove('inactive')
+});
+
+snippetOne.addEventListener('click', (e) => {
+     snippetOne.classList.add('inactive');
+     snippetTwo.classList.remove('inactive')
+});
+
+snippetTwo.addEventListener('click', (e) => {
+     snippetTwo.classList.add('inactive');
+     startGame.classList.remove('inactive')
+});
