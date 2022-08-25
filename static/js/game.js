29dd61bcaa5ae96f21741bfb5713ae5b0ad90766
eszterberenyi <e.eszterberenyi@gmail.com>
2022-08-25 11:44:43 +0200

    add new js files based on html they link to

--- static/js/game.js
+++ static/js/game.js
@@ -1,33 +1,4 @@
-function initGame() {
-
-    Splitting();
-    inputName();
-
-}
-
-const changeField = (event) => {
-    if (event.key === 'Enter') {
-        const userName = event.currentTarget.value;
-        console.log(userName);
-        document.querySelector('#inputDiv').classList.add('inactive');
-        const pDiv = document.querySelector('#pDiv');
-        const btnDiv = document.querySelector('#btnDiv');
-        const scoreLink = document.querySelector('a');
-        const hiddenInput = document.querySelector('[type=hidden]')
-        pDiv.classList.remove('inactive');
-        btnDiv.classList.remove('inactive');
-        document.querySelector('p').innerText = userName;
-        scoreLink.href = "/scores?username="+userName
-        hiddenInput.setAttribute('value', userName)
-    }
-}
-
-function inputName() {
-    const inputField = document.querySelector('#name');
-    inputField.addEventListener('keydown', changeField)
-}
 
 
 
-initGame();
 
---
+++ static/js/highScores.js
@@ -0,0 +1,22 @@
+function initPage() {
+
+    Splitting();
+    changeScoresPage();
+
+}
+
+function changeScoresPage() {
+    let params = new URLSearchParams(document.location.search);
+    if (params.get('username')) {
+        const buttons = document.querySelector('.container.btns');
+        const myScoresDiv = document.querySelector('#link2');
+        const myScoresLink = document.querySelector('#myScoresLink');
+        const goBackLink = document.querySelector('#backLink');
+        buttons.classList.remove('nouser');
+        myScoresDiv.classList.remove('inactive');
+        goBackLink.href = "/?username="+params.get('username');
+        myScoresLink.href = "/scores?username="+params.get('username')
+    }
+}
+
+initPage()
